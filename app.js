d3ac96fa9f53b0c2ddca066caab79d5b43bb4510
Roodey <joey8williams@gmail.com>
2017-05-21 16:27:55 -0400

    added heroku support

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const user = require('./pods/user/access');
+
+app.listen(3000, () => console.log('Listening on port 3000'));
+
+
+app.get('/',(req,res) => res.send('Hello World!'));
+app.use('/user',user); //Use the user routes defined in access.js
+
