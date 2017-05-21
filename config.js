356cb6345456297edfb6a5ca3b88a66351f19618
Roodey <joey8williams@gmail.com>
2017-05-21 16:38:35 -0400

    started on authentication and databasing

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret:'/Wk8SC5ma+62z/lfKjKnIrafhrDfmMPWUzbumIm3cKafLb0UqS1u3b5r6q4NCFQJ',
+    database: 'postgres://ygyvoccgmtaokk:b8d4e7c0b777155c184bccf3c2a8feb055b8e51a6f2f17351260da143e2c03e3@ec2-54-243-107-66.compute-1.amazonaws.com:5432/d4i2bef8r6sjd7'
+}
